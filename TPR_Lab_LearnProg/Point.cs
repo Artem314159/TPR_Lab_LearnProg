212ab6f735cd7fbf4c7336e6ff357c53d59ec3c7
Artem314159 <recruter@i.ua>
2018-11-21 09:48:53 +0200

    Create Point class

---
+++ TPR_Lab_LearnProg/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPR_Lab_LearnProg
+{
+    public struct Point : IEquatable<Point>
+    {
+        public double x;
+        public double y;
+
+        public Point(double x1, double y1)
+        {
+            x = x1;
+            y = y1;
+        }
+
+        public bool Equals(Point other)
+        {
+            return this.x == other.x && this.y == other.y;
+        }
+
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point left, Point right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
