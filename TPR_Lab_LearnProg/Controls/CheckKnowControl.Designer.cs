039f5bfe58b688c075ae4e946a54f99699182b47
Artem314159 <recruter@i.ua>
2018-12-27 04:57:45 +0200

    Filling checking

--- TPR_Lab_LearnProg/Controls/CheckKnowControl.Designer.cs
+++ TPR_Lab_LearnProg/Controls/CheckKnowControl.Designer.cs
@@ -28,17 +28,380 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Z_NUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Q_NUD = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tblLayPnlQ = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tblLayPnlZ = new System.Windows.Forms.TableLayoutPanel();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.PrevBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.M_NUD = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage1.SuspendLayout();
+            this.TabControl.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Z_NUD)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Q_NUD)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.M_NUD)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage5.Location = new System.Drawing.Point(4, 4);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage5.Size = new System.Drawing.Size(861, 431);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage4.Location = new System.Drawing.Point(4, 4);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage4.Size = new System.Drawing.Size(861, 431);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage3.Location = new System.Drawing.Point(4, 4);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage3.Size = new System.Drawing.Size(861, 431);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage2.Location = new System.Drawing.Point(4, 4);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage2.Size = new System.Drawing.Size(861, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.M_NUD);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.panel3);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage1.Size = new System.Drawing.Size(861, 431);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // TabControl
+            // 
+            this.TabControl.Alignment = System.Windows.Forms.TabAlignment.Right;
+            this.TabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TabControl.Appearance = System.Windows.Forms.TabAppearance.Buttons;
+            this.TabControl.Controls.Add(this.tabPage1);
+            this.TabControl.Controls.Add(this.tabPage2);
+            this.TabControl.Controls.Add(this.tabPage3);
+            this.TabControl.Controls.Add(this.tabPage4);
+            this.TabControl.Controls.Add(this.tabPage5);
+            this.TabControl.Controls.Add(this.tabPage6);
+            this.TabControl.ItemSize = new System.Drawing.Size(0, 1);
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Margin = new System.Windows.Forms.Padding(0);
+            this.TabControl.Multiline = true;
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(884, 439);
+            this.TabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.TabControl.TabIndex = 4;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage6.Location = new System.Drawing.Point(4, 4);
+            this.tabPage6.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(20);
+            this.tabPage6.Size = new System.Drawing.Size(861, 431);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "tabPage6";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(20, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(821, 81);
+            this.panel2.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Z_NUD);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(435, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 81);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Заполнение матрицы Z";
+            // 
+            // Z_NUD
+            // 
+            this.Z_NUD.Location = new System.Drawing.Point(239, 31);
+            this.Z_NUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Z_NUD.Name = "Z_NUD";
+            this.Z_NUD.Size = new System.Drawing.Size(120, 29);
+            this.Z_NUD.TabIndex = 5;
+            this.Z_NUD.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.Z_NUD.ValueChanged += new System.EventHandler(this.Z_NUD_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Строк:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Q_NUD);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(435, 81);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Заполнение матрицы Q";
+            // 
+            // Q_NUD
+            // 
+            this.Q_NUD.Location = new System.Drawing.Point(227, 31);
+            this.Q_NUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Q_NUD.Name = "Q_NUD";
+            this.Q_NUD.Size = new System.Drawing.Size(120, 29);
+            this.Q_NUD.TabIndex = 5;
+            this.Q_NUD.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.Q_NUD.ValueChanged += new System.EventHandler(this.Q_NUD_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Строк:";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.tblLayPnlQ);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(20, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 310);
+            this.panel1.TabIndex = 12;
+            // 
+            // tblLayPnlQ
+            // 
+            this.tblLayPnlQ.ColumnCount = 3;
+            this.tblLayPnlQ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlQ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlQ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlQ.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tblLayPnlQ.Location = new System.Drawing.Point(0, 0);
+            this.tblLayPnlQ.Name = "tblLayPnlQ";
+            this.tblLayPnlQ.RowCount = 1;
+            this.tblLayPnlQ.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayPnlQ.Size = new System.Drawing.Size(280, 100);
+            this.tblLayPnlQ.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.Controls.Add(this.tblLayPnlZ);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(561, 101);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(280, 310);
+            this.panel3.TabIndex = 13;
+            // 
+            // tblLayPnlZ
+            // 
+            this.tblLayPnlZ.ColumnCount = 3;
+            this.tblLayPnlZ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlZ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlZ.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblLayPnlZ.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tblLayPnlZ.Location = new System.Drawing.Point(0, 0);
+            this.tblLayPnlZ.Name = "tblLayPnlZ";
+            this.tblLayPnlZ.RowCount = 1;
+            this.tblLayPnlZ.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayPnlZ.Size = new System.Drawing.Size(280, 100);
+            this.tblLayPnlZ.TabIndex = 1;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ExitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitBtn.Location = new System.Drawing.Point(393, 452);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(80, 25);
+            this.ExitBtn.TabIndex = 7;
+            this.ExitBtn.Text = "Main Menu";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // PrevBtn
+            // 
+            this.PrevBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.PrevBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrevBtn.Location = new System.Drawing.Point(18, 452);
+            this.PrevBtn.Name = "PrevBtn";
+            this.PrevBtn.Size = new System.Drawing.Size(80, 25);
+            this.PrevBtn.TabIndex = 6;
+            this.PrevBtn.Text = "◀ Previous";
+            this.PrevBtn.UseVisualStyleBackColor = true;
+            this.PrevBtn.Click += new System.EventHandler(this.PrevBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NextBtn.Location = new System.Drawing.Point(768, 452);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(80, 25);
+            this.NextBtn.TabIndex = 5;
+            this.NextBtn.Text = "Next ▶";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // M_NUD
+            // 
+            this.M_NUD.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.M_NUD.Location = new System.Drawing.Point(414, 199);
+            this.M_NUD.Name = "M_NUD";
+            this.M_NUD.Size = new System.Drawing.Size(120, 35);
+            this.M_NUD.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(327, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 33);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "M:";
             // 
             // CheckKnowControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.PrevBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.TabControl);
             this.Name = "CheckKnowControl";
+            this.Size = new System.Drawing.Size(870, 490);
+            this.Load += new System.EventHandler(this.CheckKnowControl_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.TabControl.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Z_NUD)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Q_NUD)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.M_NUD)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TableLayoutPanel tblLayPnlZ;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tblLayPnlQ;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown Z_NUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown Q_NUD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Button PrevBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.NumericUpDown M_NUD;
+        private System.Windows.Forms.Label label3;
     }
 }
--- TPR_Lab_LearnProg/Controls/CheckKnowControl.cs
+++ TPR_Lab_LearnProg/Controls/CheckKnowControl.cs
@@ -12,9 +12,184 @@ namespace TPR_Lab_LearnProg.Controls
 {
     public partial class CheckKnowControl : UserControl
     {
+        #region Private fields
+
+        double[,] matrQ;
+        double[,] matrZ;
+
+        #endregion
+
         public CheckKnowControl()
         {
             InitializeComponent();
+            InitMatr();
+        }
+
+        private void CheckKnowControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitMatr()
+        {
+            int m = (int)Q_NUD.Value + 1, r = (int)Z_NUD.Value + 1;
+            tblLayPnlQ.RowCount = m;
+            tblLayPnlZ.RowCount = r;
+            tblLayPnlQ.RowStyles.Clear();
+            tblLayPnlZ.RowStyles.Clear();
+            tblLayPnlQ.Height = 0;
+            tblLayPnlZ.Height = 0;
+            for (int i = 0; i < m; i++)
+            {
+                tblLayPnlQ.Height += 50;
+                if (i==0)
+                {
+                tblLayPnlQ.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+                    tblLayPnlQ.Controls.Add(new Label() { Text = "β1" }, 1, i);
+                    tblLayPnlQ.Controls.Add(new Label() { Text = "β2" }, 2, i);
+                }
+                else
+                {
+                    AddRow(tblLayPnlQ, i, "α");
+                }
+            }
+            SetControlsSettings(tblLayPnlQ.Controls);
+
+            for (int i = 0; i < r; i++)
+            {
+                tblLayPnlZ.Height += 50;
+                if (i == 0)
+                {
+                tblLayPnlZ.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+                    tblLayPnlZ.Controls.Add(new Label() { Text = "β1" }, 1, i);
+                    tblLayPnlZ.Controls.Add(new Label() { Text = "β2" }, 2, i);
+                }
+                else
+                {
+                    AddRow(tblLayPnlZ, i, "z");
+                }
+            }
+            SetControlsSettings(tblLayPnlZ.Controls);
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            ControlFuncs.ChangeScene("TrainingControl", "MainMenuControl", InitFormType.InitForMainMenu);
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            int position = TabControl.TabPages.IndexOf(TabControl.SelectedTab);
+            if (TabPageCanChaged(position) && position != TabControl.TabPages.Count - 1)
+                TabControl.SelectedTab = TabControl.TabPages[position + 1];
+        }
+
+        private void PrevBtn_Click(object sender, EventArgs e)
+        {
+            int position = TabControl.TabPages.IndexOf(TabControl.SelectedTab);
+            if (TabPageCanChaged(position) && position != 0)
+                TabControl.SelectedTab = TabControl.TabPages[position - 1];
+        }
+
+        private bool tabPage1_Chaged()
+        {
+            bool res = (int)M_NUD.Value == (int)Math.Pow(tblLayPnlQ.RowCount, tblLayPnlZ.RowCount);
+            if (!res) MessageBox.Show("Value M is wrong.");
+            return res;
+        }
+
+        private bool TabPageCanChaged(int selectedTab)
+        {
+            switch (selectedTab)
+            {
+                case 0:
+                    return tabPage1_Chaged();
+                //case 1:
+                //    return tabPage2_Chaged();
+                //case 2:
+                //    return tabPage3_Chaged();
+                //case 3:
+                //    return tabPage4_Chaged();
+                //case 4:
+                //    return tabPage5_Chaged();
+                //case 5:
+                //    return tabPage6_Chaged();
+                default:
+                    return false;
+            }
+        }
+
+        private void Q_NUD_ValueChanged(object sender, EventArgs e)
+        {
+            NUD_ValueChanged(tblLayPnlQ, (int)Q_NUD.Value + 1, "α");
+        }
+
+        private void Z_NUD_ValueChanged(object sender, EventArgs e)
+        {
+            NUD_ValueChanged(tblLayPnlZ, (int)Z_NUD.Value + 1, "z");
+        }
+
+        private void NUD_ValueChanged(TableLayoutPanel tblLayPnl, int value, string text)
+        {
+            int prevRowCount = tblLayPnl.RowCount;
+            tblLayPnl.RowCount = value;
+            if (prevRowCount < tblLayPnl.RowCount)
+            {
+                for (int i = prevRowCount; i < tblLayPnl.RowCount; i++)
+                {
+                    AddRow(tblLayPnl, i, text);
+                }
+                SetControlsSettings(tblLayPnl.Controls);
+            }
+            else
+            {
+                for (int i = tblLayPnl.RowCount; i < prevRowCount; i++)
+                {
+                    DeleteRow(tblLayPnl);
+                }
+            }
+        }
+
+        private void TxtToNumber(object sender, EventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            txtBox.Text = Convert.ToDouble(txtBox.Text).ToString();
+        }
+
+        private void AddRow(TableLayoutPanel tblLayPnl, int i, string text)
+        {
+            tblLayPnl.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+            tblLayPnl.Height += 50;
+            tblLayPnl.Controls.Add(new Label() { Text = text + (i + 1) }, 0, i);
+            Control c = new TextBox() { Text = "0" };
+            c.TextChanged += TxtToNumber;
+            tblLayPnl.Controls.Add(c, 1, i);
+            c = new TextBox() { Text = "0" };
+            c.TextChanged += TxtToNumber;
+            tblLayPnl.Controls.Add(c, 2, i);
+        }
+
+        private void DeleteRow(TableLayoutPanel tblLayPnl)
+        {
+            int last = tblLayPnl.RowStyles.Count - 1;
+            tblLayPnl.RowStyles.RemoveAt(last);
+            tblLayPnl.Height -= 50;
+            foreach (Control item in tblLayPnl.Controls)
+            {
+                var b = tblLayPnl.GetRow(item);
+                if (tblLayPnl.GetRow(item) == last)
+                    tblLayPnl.Controls.Remove(item);
+            }
+        }
+
+        private void SetControlsSettings(ControlCollection controls)
+        {
+            foreach (Control control in controls)
+            {
+                if (control is Label)
+                    (control as Label).AutoSize = false;
+                control.Dock = DockStyle.Fill;
+            }
         }
     }
 }
